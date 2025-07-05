5f25c4560b3634240250b288f64ee9c27042755a
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-07-05 22:35:47 +0800

    Create math.h

---
+++ src/cubevec/math/math.h
@@ -0,0 +1,13 @@
+/*
+ CubeVec Engine Core
+ under MIT License.
+*/
+
+#include "cubevec/math/vector.h"
+#include "cubevec/math/vector_operator.h"
+#include "cubevec/math/geometry.h"
+#include "cubevec/math/roots.h"
+#include "cubevec/math/common.h"
+#include "cubevec/math/round.h"
+#include "cubevec/math/trigonometry.h"
+
